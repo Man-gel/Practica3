c24c01869eed11668db846242c7938ddb4d2d755
javieroropeza <night_captain@hotmail.com>
2014-10-14 18:57:20 -0500

    Base del codigo.

---
+++ PracticaHashTable/PracticaHashTable/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 14/10/2014
+ * Time: 06:56 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PracticaHashTable
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
