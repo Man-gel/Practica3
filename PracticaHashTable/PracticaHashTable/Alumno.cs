1f3ee73a13bb981fda4b129f9d95d60155d6858e
javieroropeza <night_captain@hotmail.com>
2014-10-14 19:52:04 -0500

    Avance del martes. Clase Alumno y clase Program con metodos para accesar al hashtable.

---
+++ PracticaHashTable/PracticaHashTable/Alumno.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 14/10/2014
+ * Time: 07:02 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PracticaHashTable
+{
+	public class Alumno
+	{
+		public String nombre;
+		public int codigo;
+		
+		public Alumno()
+		{
+			this.nombre ="";
+			this.codigo = 0;
+		}
+	}
+}
--- PracticaHashTable/PracticaHashTable/Program.cs
+++ PracticaHashTable/PracticaHashTable/Program.cs
@@ -7,16 +7,60 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections;
 
 namespace PracticaHashTable
 {
 	class Program
 	{
+		public Alumno alumno;
+		public Hashtable tabla;
+		
+		public Program()
+		{
+			this.alumno = new Alumno();
+			this.tabla = new Hashtable();
+		}
+		
+		
+		public void ingresarAlumno()
+		{
+			
+			Console.WriteLine("Codigo Alumno:");
+			alumno.codigo= Int32.Parse( Console.ReadLine() );
+			
+			Console.WriteLine("Nombre Alumno:");
+			alumno.nombre = Console.ReadLine();
+		}
+		
+		public void editarAlumno()
+		{
+			Console.WriteLine("Ingrese el codigo del alumno a editar:");
+			String codigo = Console.ReadLine();
+			if (this.tabla.ContainsKey(codigo))
+			{
+				tabla[codigo] = nombreAmodificar();
+			}
+			else
+			{
+				Console.Write("El codigo no esta registrado en la tabla !");
+			    Console.ReadKey(true);
+			}
+		}
+		
+		private String nombreAmodificar()
+		{
+			String nuevoNombre;
+			Console.WriteLine("Ingrese el Nuevo nombre:");
+			nuevoNombre = Console.ReadLine();
+			return nuevoNombre;
+		}
+		
+		
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
+			Program pruebaPractica = new Program();
+			pruebaPractica.ingresarAlumno();
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
